5156a70fa6dfac0066f871ba0d7ea799cb54a241
Jiahui Xu <jxu@ethz.ch>
2025-07-24 20:05:41 +0200

    add pragma handler struct

---
+++ tools/source-transformer/PragmaHandler.h
@@ -0,0 +1,93 @@
+// Declares clang::SyntaxOnlyAction.
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/ASTTypeTraits.h"
+#include "clang/AST/Stmt.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/Basic/DiagnosticIDs.h"
+#include "clang/Basic/SourceLocation.h"
+#include "clang/Basic/TokenKinds.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Lex/Lexer.h"
+// Declares llvm::cl::extrahelp.
+#include "clang/Lex/Pragma.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Tooling/Tooling.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Support/raw_ostream.h"
+#include <charconv>
+#include <optional>
+#include <string>
+
+using namespace clang;
+using namespace llvm;
+using namespace clang::tooling;
+
+inline std::optional<int> strToInt(const std::string &str) {
+  int value;
+  auto result = std::from_chars(str.data(), str.data() + str.size(), value);
+  if (result.ec == std::errc()) {
+    return value;
+  }
+  return std::nullopt;
+}
+
+struct UnrollPragmaInfo {
+  std::string identifier;
+  unsigned factor;
+};
+
+class UnrollPragmaHandler : public PragmaHandler {
+  std::vector<UnrollPragmaInfo> &pragmaInfo;
+
+public:
+  UnrollPragmaHandler(std::vector<UnrollPragmaInfo> &pragmaInfo)
+      : PragmaHandler("HLS_UNROLL"), pragmaInfo(pragmaInfo) {}
+
+  void HandlePragma(Preprocessor &pp, PragmaIntroducer introducer,
+                    Token &firstToken) override {
+    Token tok;
+
+    pp.Lex(tok);
+    if (tok.isNot(tok::identifier)) {
+      pp.Diag(tok.getLocation(), diag::err_expected) << "expected 'identifier'";
+      return;
+    }
+
+    std::string identifier = pp.getSpelling(tok);
+
+    // Step 1: Expect identifier "factor"
+    pp.Lex(tok);
+    if (tok.isNot(tok::identifier) || pp.getSpelling(tok) != "factor") {
+      pp.Diag(tok.getLocation(), diag::err_expected_either)
+          << "expected 'factor'";
+      return;
+    }
+
+    // Step 2: Expect '='
+    pp.Lex(tok);
+    if (tok.isNot(tok::equal)) {
+      pp.Diag(tok.getLocation(), diag::err_expected) << "'='";
+      return;
+    }
+
+    // Step 3: Expect an integer literal (e.g., 3)
+    pp.Lex(tok);
+    if (tok.isNot(tok::numeric_constant)) {
+      pp.Diag(tok.getLocation(), diag::err_expected) << "integer literal";
+      return;
+    }
+
+    // Extract the integer value
+    std::string valStr;
+    valStr = pp.getSpelling(tok);
+    auto factor = strToInt(valStr);
+
+    if (!factor) {
+      pp.Diag(tok.getLocation(), diag::err_invalid_numeric_udl);
+      return;
+    }
+
+    UnrollPragmaInfo info{identifier, (unsigned)factor.value()};
+    pragmaInfo.push_back(info);
+  }
+};
